161c723818180437501001c2be896f754e682187
corruptRadiance <50561134+corruptRadiance@users.noreply.github.com>
2022-05-09 00:51:28 -0600

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -2,46 +2,81 @@
 using System.IO;
 using System.Text;
 
-namespace StructTesting
+namespace AlbumBacklogManager
 {
     class Program
     {
-        struct Song
+        public struct Song
         {
-            public string Name;
-            public double Length;
+            public Song(string name, string artist, string album, float length, int priority)
+            {
+                Name = name;
+                Artist = artist;
+                Album = album;
+                Length = length;
+                Priority = priority;
+            }
 
-            public double LengthMinutes {get {
-                return Math.Floor(Length/60);
+            // Nullable strings is a band-aid fix for the egregious amount of
+            // possible null reference warnings I'm getting for using Console.Readline()
+            public string? Name;
+            public string? Artist;
+            public string? Album;
+            public float Length;
+            public int Priority;
+
+            public int LengthMinutes {get {
+                return (int) (Math.Floor(Length/60f));
             }}
 
-            public double LengthSeconds {get {
+            public float LengthSeconds {get {
                 return Length%60;
             }}
 
-            // Old shitty way of doing minutes
-            // public (int,int) InMinutes()
-            // {
-            //     // First array element is length/60
-            //     // Second array element is remainder
-
-            //     int minutes = Length/60;
-            //     int seconds = Length%60;
-
-            //     return (minutes,seconds);
-            // }
+            public string LengthTotal {get {
+                return LengthMinutes + ":" + LengthSeconds.ToString("00");
+            }}
         }
 
         static public void Main()
         {
-            Song songExample = new Song();
+            Song newSong = PromptNewSong();
+            Console.WriteLine(newSong.LengthTotal);
+            PromptExit();
+        }
 
-            Console.WriteLine("Length of song in seconds: ");
-            songExample.Length = double.Parse(Console.ReadLine());
+        static public void PromptExit()
+        {
+            // This is such a tiny thing but for some reason
+            // having these two lines in Main() always annoys me
 
-            Console.WriteLine("Song Length: " + songExample.LengthMinutes + ":" + songExample.LengthSeconds.ToString("00"));
-
+            Console.Write("\nPress Enter to exit.");
             Console.ReadLine();
+        }
+
+        static public Song PromptNewSong()
+        {
+            // Probably won't prove super useful as time goes on
+            // Good for testing
+            
+            Song s = new Song();
+
+            Console.Write("Name of song: ");
+            s.Name = Console.ReadLine();
+
+            Console.Write("Name of artist: ");
+            s.Artist = Console.ReadLine();
+
+            Console.Write("Name of album: ");
+            s.Album = Console.ReadLine();
+
+            Console.Write("Length of song (seconds): ");
+            s.Length = int.Parse(Console.ReadLine());
+
+            Console.Write("Priority Level (0 - 3): ");
+            s.Priority = int.Parse(Console.ReadLine());
+
+            return s;
         }
     }
 }
