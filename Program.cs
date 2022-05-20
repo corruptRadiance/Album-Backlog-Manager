ec51f2512a4ad62c50d1e3ec3ae21464f6250277
corruptRadiance <50561134+corruptRadiance@users.noreply.github.com>
2022-05-19 21:34:08 -0600

    Reorganizing
    Separated Program.cs
    Songs.cs will contain all elements of song management

--- Program.cs
+++ Program.cs
@@ -1,47 +1,35 @@
 ﻿using System;
 using System.IO;
 using System.Text;
-using SpotifyAPI.Web; // For later
+using AlbumBacklogManager.SongManagement;
 
 namespace AlbumBacklogManager
 {
     class Program
     {
-        public struct Song
-        {
-            public Song(string name, string artist, string album, float length, int priority)
-            {
-                Name = name;
-                Artist = artist;
-                Album = album;
-                Length = length;
-                Priority = priority;
-            }
-
-            // Nullable strings is a band-aid fix for the egregious amount of
-            // possible null reference warnings I'm getting for using Console.Readline()
-            public string? Name;
-            public string? Artist;
-            public string? Album;
-            public float Length;
-            public int Priority;
-
-            public int LengthMinutes {get {
-                return (int) (Math.Floor(Length/60f));
-            }}
-
-            public float LengthSeconds {get {
-                return Length%60;
-            }}
-
-            public string LengthTotal {get {
-                return LengthMinutes + ":" + LengthSeconds.ToString("00");
-            }}
-        }
+        // Acceptable responses for Y/N prompts
+        static public string[] Responses = new string[]{"y", "yes"};
 
         static public void Main()
         {
-            
+            // This needs to get wrapped in a loop probably
+
+            // Prompt and store user input 
+            string input;
+            Console.Write("Would you like to add a new song? (Y/N): ");
+            input = Console.ReadLine().ToLower();
+
+            // Check if user input is within acceptable "true" responses
+            foreach(string i in Responses)
+            {
+                // If so, prompt user further
+                if(input.Contains(i))
+                {
+                    SongManager.PromptNewSong();
+                }
+            }
+
+            // Prompt user to exit app when finished
             PromptExit();
         }
 
@@ -52,31 +40,6 @@ namespace AlbumBacklogManager
 
             Console.Write("\nPress Enter to exit.");
             Console.ReadLine();
-        }
-
-        static public Song PromptNewSong()
-        {
-            // Probably won't prove super useful as time goes on
-            // Good for testing
-            
-            Song s = new Song();
-
-            Console.Write("Name of song: ");
-            s.Name = Console.ReadLine();
-
-            Console.Write("Name of artist: ");
-            s.Artist = Console.ReadLine();
-
-            Console.Write("Name of album: ");
-            s.Album = Console.ReadLine();
-
-            Console.Write("Length of song (seconds): ");
-            s.Length = int.Parse(Console.ReadLine());
-
-            Console.Write("Priority Level (0 - 3): ");
-            s.Priority = int.Parse(Console.ReadLine());
-
-            return s;
         }
     }
 }
---
+++ Songs.cs
@@ -0,0 +1,72 @@
+using System;
+using System.IO;
+using System.Text;
+using SpotifyAPI.Web; // For later
+
+namespace AlbumBacklogManager.SongManagement
+{
+    static public class SongManager
+    {
+        static public Song PromptNewSong()
+        {
+            // Probably won't prove super useful as time goes on
+            // Good for testing
+
+            Song s = new Song();
+
+            Console.Write("Name of song: ");
+            s.Name = Console.ReadLine();
+
+            Console.Write("Name of artist: ");
+            s.Artist = Console.ReadLine();
+
+            Console.Write("Name of album: ");
+            s.Album = Console.ReadLine();
+
+            Console.Write("Length of song (seconds): ");
+            s.Length = int.Parse(Console.ReadLine());
+
+            Console.Write("Priority Level (0 - 3): ");
+            s.Priority = int.Parse(Console.ReadLine());
+
+            return s;
+        }
+    }
+    public struct Song
+    {
+        public Song(string name, string artist, string album, float length, int priority)
+        {
+            // This looks really ugly to me but y'know it's w/e
+            Name = name;
+            Artist = artist;
+            Album = album;
+            Length = length;
+            Priority = priority;
+        }
+
+        // Nullable strings is a band-aid fix for the egregious amount of
+        // possible null reference warnings I'm getting for using Console.Readline()
+        public string? Name;
+        public string? Artist;
+        public string? Album;
+        public float Length; // Consider using TimeSpan for this
+
+        public int Priority{set{
+            if (value < 0 || value > 3)
+            {
+                throw new ArgumentOutOfRangeException($"{nameof(value)} must be between 0 and 3.");
+            }}}
+
+        public int LengthMinutes{get{
+            return (int)(Math.Floor(Length / 60f));
+            }}
+
+        public float LengthSeconds{get{
+            return Length % 60f;
+            }}
+
+        public string LengthTotal{get{
+            return LengthMinutes + ":" + LengthSeconds.ToString("00");
+            }}
+    }
+}
